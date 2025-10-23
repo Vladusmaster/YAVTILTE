c707190c21c317cc7837297df6338703915d80c6
Vladus <96520195+Vladusmaster@users.noreply.github.com>
2025-10-23 13:12:13 +0300

    Add files via upload

---
+++ 2/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Мультисписок
+        List<List<Integer>> multiList = new ArrayList<>();
+
+        // Очередь
+        Queue<Integer> queue = new LinkedList<>();
+
+        // Дек (двусторонняя очередь)
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        // Приоритетная очередь
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+    }
+}
